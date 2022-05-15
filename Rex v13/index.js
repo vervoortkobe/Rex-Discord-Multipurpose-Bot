bffdeb37d6db01f4d0b95d29fe3220b08156a3d0
Tsunami <39643347+Tsunami2360@users.noreply.github.com>
2022-05-15 13:50:12 +0200

    Update index.js

--- Rex v13/index.js
+++ Rex v13/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.send("online");
 });
 
 app.use(cors());
