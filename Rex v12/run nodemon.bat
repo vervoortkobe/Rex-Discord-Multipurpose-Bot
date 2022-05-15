dea3ffe74aac22434a19935046b6242c147e4ee8
Tsunami <39643347+Tsunami2360@users.noreply.github.com>
2022-05-15 13:32:59 +0200

    Update and rename v12/run nodemon.bat to Rex v12/run nodemon.bat

---
+++ Rex v12/run nodemon.bat
@@ -0,0 +1,4 @@
+@echo on
+cd D:\Downloads\.Main\Andere\nodejs\Bots\Rex v1.12
+nodemon index.js
+pause
--- v12/run nodemon.bat
+++
@@ -1,4 +0,0 @@
-@echo on
-cd D:\Downloads\KOBE Downloads\Andere\Bots\Rex v1.12
-nodemon index.js
-pause
