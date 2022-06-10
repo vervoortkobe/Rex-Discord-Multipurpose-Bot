fb00f704349428d6cf70de86ac4b06735479612d
Tsunami <39643347+Tsunami2360@users.noreply.github.com>
2022-06-10 14:46:16 +0200

    Add files via upload

---
+++ Rex v1.10.4/commands/goose.js
@@ -0,0 +1,29 @@
+const config = require("../config.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const fetch = require("node-fetch");
+const superagent = require("superagent");
+const math = require("mathjs");
+const prefix = (config.prefix);
+
+module.exports.run = async (client, message, args) => {
+
+    let {data} = await fetch;
+
+    fetch('https://nekos.life/api/v2/img/goose')
+    .then(res => res.json()).then(data => {
+      if(!data) return message.channel.send(`❌ | There went something wrong! Pls try again!`);
+
+    const gooseEmbed = new Discord.RichEmbed()
+    .setColor(0xff0000)
+    .setTitle(`🦆 | ${client.user.username} Goose`)
+    .setImage(data.url)
+    .setFooter(`© ${client.user.username} was made by Tsunami#6271`)
+    .setTimestamp()
+    message.channel.send(gooseEmbed);
+    })
+  }
+
+  module.exports.help = {
+    name: "goose"
+}
