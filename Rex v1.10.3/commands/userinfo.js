07ba2154bc1e7fcaa16e6c5c56327cd0e53284b2
Tsunami <39643347+Tsunami2360@users.noreply.github.com>
2022-06-10 14:41:18 +0200

    Add files via upload

---
+++ Rex v1.10.3/commands/userinfo.js
@@ -0,0 +1,38 @@
+const config = require("../config.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const fetch = require("node-fetch");
+const superagent = require("superagent");
+const math = require("mathjs");
+const prefix = (config.prefix);
+
+module.exports.run = async (client, message, args) => {
+
+    function checkDays(date) {
+        let now = new Date();
+        let diff = now.getTime() - date.getTime();
+        let days = Math.floor(diff / 86400000);
+        return days + (days == 1 ? " day" : " days") + " ago";
+      };
+
+    let memberToFind = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if (!memberToFind) {
+      memberToFind = message.author
+      }
+
+      let userinfoEmbed = new Discord.RichEmbed()
+          .setColor(0xff0000)
+          .setTitle(`👤 | ${client.user.username} Userinfo`)
+          .setThumbnail(memberToFind.avatarURL)
+          .addField(`User Name`, memberToFind.tag, true)
+          .addField(`Account Creation Date`, `${memberToFind.createdAt} (${checkDays(memberToFind.createdAt)})`, true)
+          .addField(`Server Join Date`, message.guild.members.find('id', memberToFind.id).joinedAt, true)
+          .addField(`User ID`, memberToFind.id, true)
+          .setFooter(`© ${client.user.username} was made by Tsunami#6271`)
+          .setTimestamp()
+      message.channel.send(userinfoEmbed);
+}
+
+module.exports.help = {
+    name: "userinfo"
+}
